b217fb67e5772f9a04c8f0eceff13677a5039634
Glitch (courageous-hole) <none>
2019-03-13 15:16:48 +0000

    Stuck on 2nd exercise at the moment. Forum notes some problems.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
@@ -17,7 +20,10 @@ app.set('view engine', 'pug')
 app.route('/')
   .get((req, res) => {
     res.sendFile(process.cwd() + '/views/pug/index.pug');
+    res.render(process.cwd() + '/views/pug/index', {title: 'Home page', message: 'Please login'});
   });
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
