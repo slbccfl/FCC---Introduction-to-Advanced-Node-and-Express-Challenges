41dd6464f670da0e11a5275616986079166609a1
Glitch (courageous-hole) <none>
2019-03-28 12:45:32 +0000

    Progressing with authetication and implemented modules

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 'use strict';
 
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session');
-const passport    = require('passport');
 const mongodb     = require('mongodb');
 const mongo = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -24,88 +23,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 
 mongo.connect(process.env.DATABASE, (err, db) => {
     if(err) {
         console.log('Database error: ' + err);
     } else {
       console.log('Successful database connection');
+      auth(app, db)
+      routes(app, db)
 
-        //serialization and app.listen
-      
-      passport.deserializeUser((id, done) => {
-        db.collection('users').findOne(
-          {_id: new ObjectID(id)},
-          (err, doc) => {
-              done(null, doc);
-          }
-        );
-      });
-      
-      passport.serializeUser((user, done) => {
-        done(null, user._id);
-      });
-      
-      passport.use(new LocalStrategy(
-        function(username, password, done) {
-          db.collection('users').findOne({ username: username }, function (err, user) {
-            console.log('User '+ username +' attempted to log in.');
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (password !== user.password) { return done(null, false); }
-            return done(null, user);
-          });
-        }
-      ));
-      
-      // app.use((req, res, next) => {
-      //   res.status(404)
-      //     .type('text')
-      //     .send('Not Found');
-      // });
-      
-      function ensureAuthenticated(req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect('/');
-      };
-      
-      app.route('/')
-        .get((req, res) => {
-          // res.sendFile(process.cwd() + '/views/pug/index.pug');
-          res.render(process.cwd() + '/views/pug/index', {title: 'Home page', message: 'Please login', showLogin: true});
-        });
-      
-      app.route('/login')
-        .post(passport.authenticate('local', { failureRedirect: '/' }),(req,res) => {
-             res.redirect('/profile');
-        });
-      
-      app.route('/profile')
-        .get(ensureAuthenticated, (req,res) => {
-             res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
-        });
-      
-      app.route('/logout')
-        .get((req, res) => {
-            req.logout();
-            res.redirect('/');
-        });
       
       app.listen(process.env.PORT || 3000, () => {
-        console.log("Listening on port " + process.env.PORT);
-      });
-      
+          console.log("Listening on port " + process.env.PORT);
+        });
+
     }
 });
 
