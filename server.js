4fe77568d4df1e99b58994a4427c6a5247a110d5
Glitch (courageous-hole) <none>
2019-03-30 14:11:42 +0000

    Redid last Implementation of Social Authentication exercise to correct placement of new auth code

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session     = require('express-session');
 const mongodb     = require('mongodb');
 const mongo = require('mongodb').MongoClient;
 const GitHubStrategy = require('passport-github').Strategy;
+// const io = require('socket.io')(http);
 
 const passport    = require('passport');
 const bcrypt     = require('bcrypt');
@@ -23,36 +24,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'https://courageous-hole.glitch.me/auth/github/callback'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      //Database logic here with callback containing our user object
-      db.collection('socialusers').findAndModify(
-        {id: profile.id},
-        {},
-        {$setOnInsert:{
-            id: profile.id,
-            name: profile.displayName || 'John Doe',
-            photo: profile.photos[0].value || '',
-            email: profile.emails[0].value || 'No public email',
-            created_on: new Date(),
-            provider: profile.provider || ''
-        },$set:{
-            last_login: new Date()
-        },$inc:{
-            login_count: 1
-        }},
-        {upsert:true, new: true},
-        (err, doc) => {
-            return cb(null, doc.value);
-        }
-    );
-  }
-));
 
 // set 'pug' as the 'view-engine'. 
 app.set('view engine', 'pug');
@@ -63,9 +34,70 @@ mongo.connect(process.env.DATABASE, (err, db) => {
     } else {
       console.log('Successful database connection');
       
+      passport.use(new GitHubStrategy({
+          clientID: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          callbackURL: 'https://courageous-hole.glitch.me/auth/github/callback'
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            //Database logic here with callback containing our user object
+            db.collection('socialusers').findAndModify(
+              {id: profile.id},
+              {},
+              {$setOnInsert:{
+                  id: profile.id,
+                  name: profile.displayName || 'John Doe',
+                  photo: profile.photos[0].value || '',
+                  email: profile.emails[0].value || 'No public email',
+                  created_on: new Date(),
+                  provider: profile.provider || ''
+              },$set:{
+                  last_login: new Date()
+              },$inc:{
+                  login_count: 1
+              }},
+              {upsert:true, new: true},
+              (err, doc) => {
+                  return cb(null, doc.value);
+              }
+          );
+        }
+      ));
+      
       auth(app, db)
       routes(app, db)
   
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://courageous-hole.glitch.me/auth/github/callback'
+      },
+      function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          //Database logic here with callback containing our user object
+          db.collection('socialusers').findAndModify(
+            {id: profile.id},
+            {},
+            {$setOnInsert:{
+                id: profile.id,
+                name: profile.displayName || 'John Doe',
+                photo: profile.photos[0].value || '',
+                email: profile.emails[0].value || 'No public email',
+                created_on: new Date(),
+                provider: profile.provider || ''
+            },$set:{
+                last_login: new Date()
+            },$inc:{
+                login_count: 1
+            }},
+            {upsert:true, new: true},
+            (err, doc) => {
+                return cb(null, doc.value);
+            }
+        );
+      }
+    ));
       app.route('/auth/github')
         .get((req, res) => {
           res.redirect(passport.authenticate('github'))
